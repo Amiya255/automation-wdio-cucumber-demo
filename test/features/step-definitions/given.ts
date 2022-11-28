75b5d8f88683a401e038aa76dc9913659f843d28
Amiya255 <amiyabalabehera@gmail.com>
2022-11-28 02:50:49 +0000

    code cleanup

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -3,8 +3,6 @@ import chai from "chai";
 import HomePage from '../../page-objects/home-page';
 
 Given(/^Home Page is opened$/, async function () {
-  await browser.url("https://www.easyfundraising.org.uk");
-  //await HomePage.open;
-  console.log("Opened Browser successfully");
-  await browser.pause(2000);
+   //await HomePage.open; 
+  await browser.url("https://www.easyfundraising.org.uk");  
 });
