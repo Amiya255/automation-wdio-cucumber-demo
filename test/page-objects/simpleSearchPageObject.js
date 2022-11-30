e39026e8b2857fca74a453d2c3cedab560a0bedb
Amiya255 <amiyabalabehera@gmail.com>
2022-11-30 21:24:09 +0000

    typescript repalced with javascript

---
+++ test/page-objects/simpleSearchPageObject.js
@@ -0,0 +1,27 @@
+class SimpleSearchPage {
+  open() {
+    return browser.url("https://www.easyfundraising.org.uk/");
+  }
+
+  get findACauseLink() {
+    return $('div[data-testid="header-link"]:nth-child(2)');
+  }
+
+  get searchCauseLink() {
+    return $("#sagc-hero-search-input");
+  }
+
+  get searchSuggestions() {
+    return $$("#sagc-hero-search-input-auto-suggest > li > button:first-child");
+  }
+
+  get submitSearchCauseLink() {
+    return $("#sagc-hero-search-submit");
+  }
+
+  get searchCauseResult() {
+    return $$('div[data-testid="cause_details"] p[data-testid="title"]');
+  }
+}
+
+module.exports = new SimpleSearchPage();
